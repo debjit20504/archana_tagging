9b984f22f47c0423437642ae77495a07d95d6fa8
Debjit Pramanik <79047521+debjit20504@users.noreply.github.com>
2022-03-10 17:34:10 +0530

    Update archana_tagging.py

--- archana_tagging.py
+++ archana_tagging.py
@@ -1,17 +1,21 @@
-# archana.csv
-import pandas as pd
-import numpy as np
 import time
 import csv
 
 def main():
 	s_time = time.time() # time taken to read data
-	ingredient_list = []
-	with open("archana.csv", "r", encoding = "utf8") as file:
+	unwanted_items = {'Salt to taste', 'Salt-As needed', 'Water as needed', 'Salt as needed', 'Ghee as required', 'Oil as needed', ''}
+	with open("ingredients.csv", "r", encoding = "utf8") as file:
 		reader = csv.reader(file)
-		for i in reader:
-			if len(i) != 0:
-				ingredient_list.append(i[3])		
+		ing_list = []
+		lst = list(reader)
+		for i in range(len(lst)):
+			temp = str(lst[i])
+			temp = temp.strip("['']")
+			ing_list.append(temp)
+		ing_list = [i for i in ing_list if i not in unwanted_items]
+		for i in ing_list:
+			print(i)
+				
 	e_time = time.time()
 	print()
 	print("Time taken: ", (e_time-s_time), "seconds")
